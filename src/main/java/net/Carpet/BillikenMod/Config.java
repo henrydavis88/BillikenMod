cf6487decaae4caaa2014c3f7e3e98cdd062d667
henrydavis88 <henryjdavis88@gmail.com>
2024-12-31 12:07:24 -0600

    config

--- src/main/java/net/Carpet/BillikenMod/Config.java
+++ src/main/java/net/Carpet/BillikenMod/Config.java
@@ -15,7 +15,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 // An example config class. This is not required, but it's a good idea to have one to keep your config organized.
-// Demonstrates how to use Forge's config APIs
+// Demonstrates how to use Forge's config API
 @Mod.EventBusSubscriber(modid = BillikenMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class Config {
     private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
